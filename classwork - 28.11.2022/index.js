ba0215553c65f953b18719b27b08cb83b279243d
yanivcohen223 <yanivcohen223@gmail.com>
2022-11-28 19:47:28 +0200

    upload

--- classwork - 28.11.2022/index.js
+++ classwork - 28.11.2022/index.js
@@ -8,3 +8,27 @@ const fs = require('fs');
 const files = fs.readdirSync('./');
 
 console.log(`[sync] ${files}`);
+
+const sqlite3 = require('sqlite3').verbose();
+const db_file_loc = './DataBase/db1.db'
+
+const db = new sqlite3.Database (db_file_loc, (err) => {
+    if (err) {
+        console.log(`failed: ${err}, ${db_file_loc}`);
+    }
+    else {
+        console.log(`succsessfully connected to ${db_file_loc}`);
+    }
+})
+
+db.serialize(() => {
+    db.each (`SELECT * FROM COMPANY
+              WHERE SALARY > 30000;`, (err, row) => {
+                if (err) {
+                    console.log(err);
+                }
+                else {
+                    console.log(row);
+                }
+              })  
+})
