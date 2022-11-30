50603adf8fd82a789b4dc03bd5719d0062605dc2
yanivcohen223 <yanivcohen223@gmail.com>
2022-11-30 19:00:22 +0200

    upload

---
+++ homework - 29.11.2022/book shop/books_dal.js
@@ -0,0 +1,41 @@
+const sqlite3 = require('sqlite3').verbose();
+const db_file_loc = './database/books_db.db'
+
+function open_db(file_name) {
+    return new sqlite3.Database(file_name, (err) => {
+        if (err) {
+            console.log(`Failed to connect to ${file_name}`);
+        }
+        else {
+            console.log(`Successfully connected to ${file_name}`);
+        }
+    })
+}
+
+
+
+function get_all () {
+
+}
+
+function get_by_id () {
+
+}
+
+function find_by_title () {
+
+}
+
+function insert () {
+
+}
+
+function update () {
+
+}
+
+function delete_by_id () {
+
+}
+
+const db = open_db(db_file_loc)
---
+++ homework - 29.11.2022/book shop/orders_dal.js
@@ -0,0 +1,30 @@
+
+
+function open_db() {
+    
+}
+
+function get_all () {
+
+}
+
+function get_by_id () {
+
+}
+
+function find_by_lname () {
+
+}
+
+function insert () {
+
+}
+
+function update () {
+
+}
+
+function delete_by_id () {
+
+}
+
