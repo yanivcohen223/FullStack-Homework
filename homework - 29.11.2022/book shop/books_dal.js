44a392a191bad21d9cea019004a48d9fc9f47878
yanivcohen223 <yanivcohen223@gmail.com>
2022-11-30 20:13:56 +0200

    upload

--- homework - 29.11.2022/book shop/books_dal.js
+++ homework - 29.11.2022/book shop/books_dal.js
@@ -12,14 +12,33 @@ function open_db(file_name) {
     })
 }
 
-
-
-function get_all () {
-
+function get_all (db) {
+    db.serialize(() => {
+        db.each (`SELECT * FROM Books;`, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.table(row);
+                    }
+                  })  
+    })
 }
 
-function get_by_id () {
-
+function get_by_id (db, id) {
+    const select_by_id = `SELECT * FROM Books
+                            WHERE id = ?`
+    db.serialize(() => {
+        db.each (`SELECT * FROM Books
+                    WHERE id = ${id}`, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.table(row);
+                    }
+                  })  
+    })
 }
 
 function find_by_title () {
@@ -38,4 +57,20 @@ function delete_by_id () {
 
 }
 
+function close_db(db) {
+    db.close(err => {
+        if (err) {
+            console.log(err.message);
+        }
+        else {
+            console.log('Database closed!');
+        }
+    })
+}
+
 const db = open_db(db_file_loc)
+
+setTimeout(() => {get_all (db)}, 500);
+setTimeout(() => {get_by_id (db, id)}, 500);
+
+setTimeout(() => {close_db(db)}, 1000);
--- homework - 29.11.2022/book shop/orders_dal.js
+++ homework - 29.11.2022/book shop/orders_dal.js
@@ -1,7 +1,15 @@
+const sqlite3 = require('sqlite3').verbose();
+const db_file_loc = './database/orders_db.db'
 
-
-function open_db() {
-    
+function open_db(file_name) {
+    return new sqlite3.Database(file_name, (err) => {
+        if (err) {
+            console.log(`Failed to connect to ${file_name}`);
+        }
+        else {
+            console.log(`Successfully connected to ${file_name}`);
+        }
+    })
 }
 
 function get_all () {
@@ -28,3 +36,4 @@ function delete_by_id () {
 
 }
 
+const db = open_db(db_file_loc)
