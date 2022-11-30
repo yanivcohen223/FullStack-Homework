9ba18a36e82b26118c796d5a79774e65ac2aa055
yanivcohen223 <yanivcohen223@gmail.com>
2022-11-30 21:10:27 +0200

    upload

--- homework - 29.11.2022/book shop/books_dal.js
+++ homework - 29.11.2022/book shop/books_dal.js
@@ -41,12 +41,22 @@ function get_by_id (db, id) {
     })
 }
 
-function find_by_title () {
-
+function find_by_title (db, find_title) {
+    db.serialize(() => {
+        db.each (`SELECT * FROM Books 
+                    WHERE title GLOB '${find_title}*';`, (err, row) => {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log(row);
+                    }
+                  })  
+    })
 }
 
 function insert (db, data) {
-    const insert_book = `INSERT INTO COMPANY (title,author,publish_year,price,left_in_stock,book_image_src)
+    const insert_book = `INSERT INTO Books (title,author,publish_year,price,left_in_stock,book_image_src)
                         VALUES (?, ?, ?, ?, ?, ? );`
 
     db.run (insert_book, data, (err) => {
@@ -54,17 +64,29 @@ function insert (db, data) {
             console.log(err);
         }
         else {
-            console.log('succseed' + ' ' + data);
+            console.log('insert succeeded' + ' ' + data);
         }
     })
 }
 
-function update () {
+function update (db, where_to_up, what_to_up, id) {
+    let update_book = `UPDATE Books
+                         SET ${where_to_up} = ?
+                         WHERE id = ?`
 
+    db.run(update_book, what_to_up, id, err => {
+
+        if(err) {
+            console.log('error' + ' ' + err);
+        }
+        else {
+            console.log('value updated' + get_by_id (db, id) );
+        }
+    }) 
 }
 
 function delete_by_id (db, id) {
-    let delete_book = `DELETE FROM COMPANY
+    let delete_book = `DELETE FROM Books
                       WHERE ID = ?`
 
     db.run(delete_book,id, err => {
@@ -90,7 +112,10 @@ function close_db(db) {
 
 const db = open_db(db_file_loc)
 
-setTimeout(() => {get_all (db)}, 500);
-setTimeout(() => {get_by_id (db, id)}, 500);
-setTimeout(() => {delete_by_id (db, id)}, 500);
+//setTimeout(() => {get_all (db)}, 500);
+//setTimeout(() => {get_by_id (db, 1)}, 500);
+//setTimeout(() => {find_by_title (db, 'li')}, 500);
+//setTimeout(() => {insert (db, ['lion king', 'bibi', 2000, 20,3])}, 500);
+//setTimeout(() => {update (db,'author', 'idan', 1)}, 500);
+//setTimeout(() => {delete_by_id (db, id)}, 500);
 setTimeout(() => {close_db(db)}, 1000);
